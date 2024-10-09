bf3fa0b5faf1fda177c152c0d3461cc59b3e587e
lavanyaj3 <lavanya.j3@ibm.com>
2024-10-09 13:23:47 +0530

    disabling the display message (Will be pulled automatically) while installing (#71)
    Signed-off-by: lavanyaj3 <lavanya.j3@ibm.com>

--- webviews/src/ModelList.tsx
+++ webviews/src/ModelList.tsx
@@ -97,7 +97,8 @@ const ModelList: React.FC<ModelListProps> = ({ className, label, value, onChange
                         styles={customStyles}
                         formatOptionLabel={formatOptionLabel}
                     />
-                    {!status && <span className='info-label' style={{ display: 'flex', alignItems: 'center' }}> (will be pulled automatically)</span>}
+                    {!status && !progress && <span className='info-label' style={{ display: 'flex', alignItems: 'center' }}> (will be pulled automatically)</span>}
+
                 </div>
             </div>
 
