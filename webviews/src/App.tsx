c0fc3741dbb209d69fd7277b9345b0a183102d44
athirakm94 <Athira.K.M@ibm.com>
2024-10-15 18:34:13 +0530

    adding meaningful description for wizard page (#87)
    Signed-off-by: Athira K M <Athira.K.M@ibm.com>
    Signed-off-by: Fred Bricon <fbricon@gmail.com>
    Co-authored-by: Fred Bricon <fbricon@gmail.com>

--- webviews/src/App.tsx
+++ webviews/src/App.tsx
@@ -186,7 +186,10 @@ function App() {
   return (
     <main className="main-wrapper">
       <h1 className="main-title">Setup IBM Granite Code as your code assistant with Continue</h1>
-
+      <div className="main-description">
+        <p className="m-0 mb-1">Run <a href="https://github.com/ibm-granite/granite-code-models" target="_blank" rel="noopener noreferrer">IBM Granite Code</a> models effortlessly with <a href="https://github.com/ollama/ollama" target="_blank" rel="noopener noreferrer"> Ollama</a> and <a href="https://github.com/continuedev/continue" target="_blank" rel="noopener noreferrer">Continue.dev</a>.
+          Granite will help you write, generate, explain or document code, while your data stays secure and private on your own machine.</p>
+      </div>
       <div className="form-group-wrapper">
         <div className="form-group">
           <div className="ollama-status-wrapper">
